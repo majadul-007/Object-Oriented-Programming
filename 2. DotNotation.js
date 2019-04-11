f0980d4cf45e26c78a135313a20167898a7bdf7b
Tusar Haque <tusarhaque12@gmail.com>
2019-04-12 02:49:35 +0600

    Accessing Object Using Dot Notaion

---
+++ 2. DotNotation.js
@@ -0,0 +1,11 @@
+let dog = {
+  name: "Spot",
+  numLegs: 4
+};
+
+// Access An Object properties Using Dot Notation
+// Just simply add dot after the Object Name then the Property Name You Want to Access 
+// Used Console.log to Print the value
+
+  console.log(dog.name);
+  console.log(dog.numLegs);
