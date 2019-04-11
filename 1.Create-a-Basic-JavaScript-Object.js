12f20c509e19c9e2edfd9cff574380bc3260146f
Tusar Haque <tusarhaque12@gmail.com>
2019-04-12 02:26:04 +0600

    Basic javascript object

---
+++ 1.Create-a-Basic-JavaScript-Object.js
@@ -0,0 +1,10 @@
+let dog = {
+
+// Creating Object with name and numLegs
+// Object must be end with comma not semicolon 
+// Using comma  before last object not necessari
+
+    name: "doggy",
+    numLegs: 2,
+  
+};
