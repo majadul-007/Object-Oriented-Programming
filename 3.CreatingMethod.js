edf53539b28649c9b34cb04a304f9f5db4c4cadf
Tusar Haque <tusarhaque12@gmail.com>
2019-04-12 03:09:27 +0600

    Creating Method On an Object

---
+++ 3.CreatingMethod.js
@@ -0,0 +1,15 @@
+let dog = {
+  name: "Spot",
+  numLegs: 4,
+
+ // Creating sayLegs method
+
+  sayLegs: function(){
+    return "This dog has 4 legs.";
+  }
+  
+};
+
+// Accesing the Method
+
+console.log(dog.sayLegs());
