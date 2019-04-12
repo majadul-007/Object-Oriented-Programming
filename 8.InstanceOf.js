e5e3afe384b1f9bf5d1a8d74cc02fb56dad93935
Tusar Haque <tusarhaque12@gmail.com>
2019-04-13 04:16:51 +0600

    Uses Of InstanceOf

---
+++ 8.InstanceOf.js
@@ -0,0 +1,11 @@
+/* jshint expr: true */
+
+function House(numBedrooms) {
+  this.numBedrooms = numBedrooms;
+}
+
+        const myHouse = new House(4);
+
+ // Checking the code to Verfiy it is instace of The Constructor Function
+
+        console.log(myHouse instanceof House);
