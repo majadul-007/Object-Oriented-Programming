10db5542f066066d7ace3e734521131b177c3c83
Tusar Haque <tusarhaque12@gmail.com>
2019-04-12 15:50:13 +0600

    OBject Constructor

---
+++ 6.CreateObjectConstructor.js
@@ -0,0 +1,10 @@
+function Dog() {
+  this.name = "Rupert";
+  this.color = "brown";
+  this.numLegs = 4;
+}
+// Assigning Dog Constructor Function  To a A variable
+// You Must Need to Use New KeyWord for Accesing the Dog Constructor Function
+
+const hound = new Dog();
+
