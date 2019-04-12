b646a44c654d753d95eede06f8b82ef6a9946113
Tusar Haque <tusarhaque12@gmail.com>
2019-04-12 15:43:24 +0600

    Constructor Function

---
+++ 5.ConstructorFunction.js
@@ -0,0 +1,14 @@
+// Constructor function must be defined with Capitalized letter
+// Must need to use this keyword for creating Cbject Properties
+// 
+
+ function Dog(){
+      this.name = "puppy",
+      this.color = "red",
+      this.numLegs = 4;
+  }
+
+// Accesing the Function  Using new keyword and set it to a Variable
+
+   const data = new Dog();
+console.log(data.name);
